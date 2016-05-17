8cc0ebb4001b56ff0afe9453a34eadde22bec9a9
pterriquez <pterriquez@ucol>
2016-05-17 08:36:20 -0500

    Problema 01,02

---
+++ Mate/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mate
+{
+    class Program
+    {
+
+        static public int errores = 0;
+        static public int successfules = 0;
+
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Users\\Terriquez\\Documents\\pruebas.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                        if (line != "")
+                            operacion(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n*Falla*. \nEl archivo no existe, o no se puede leer esta sección.");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("\n-----------------------------------------------------------\nRESULTADOS: \n");
+                Console.WriteLine("ÉXITOS " + successfules);
+                Console.WriteLine("ERRORES " + errores);
+                Console.WriteLine("\n-----------------------------------------------------------\n");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void operacion(String line)
+        {
+            double resultadoEsperado = Double.Parse(line.Substring(line.LastIndexOf(':') + 1));
+            int[] arregloDeEnteros = stringToArray(line);
+
+            if (line.Contains("mediaAritmetica"))
+                if (medias.mediaAritmetica(arregloDeEnteros) == resultadoEsperado)
+                {
+                    Console.WriteLine(line + "\nÉxito! Valor Calculado = " + resultadoEsperado + "\n");
+                    successfules++;
+                }
+                else
+                {
+                    Console.WriteLine(line + "\n*Falla* Media aritmética Esperada = " + resultadoEsperado + ", Calculado = " + medias.mediaAritmetica(arregloDeEnteros) + "\n");
+                    errores++;
+                }
+            else if (line.Contains("mediaGeometrica"))
+                if (medias.mediaGeometrica(arregloDeEnteros) == resultadoEsperado)
+                {
+                    Console.WriteLine(line + "\nÉxito!Valor Calculado = " + resultadoEsperado + "\n");
+                    successfules++;
+                }
+                else
+                {
+                    Console.WriteLine(line + "\n*Falla* Media geométrica Esperada = " + resultadoEsperado + ", Calculado = " + medias.mediaGeometrica(arregloDeEnteros) + "\n");
+                    errores++;
+                }
+            else if (medias.mediaArmonica(arregloDeEnteros) == resultadoEsperado)
+            {
+                Console.WriteLine(line + "\nÉxito! Valor Calculado = " + resultadoEsperado + "\n");
+                successfules++;
+            }
+            else
+            {
+                Console.WriteLine(line + "\n*Falla* Media armónica Esperada = " + resultadoEsperado + ", Calculado = " + medias.mediaArmonica(arregloDeEnteros) + "\n");
+                errores++;
+            }
+        }
+
+        static int[] stringToArray(String line)
+        {
+            String recortada = line.Substring(line.IndexOf(':', 5) + 1, line.Substring(line.IndexOf(':', 5)).LastIndexOf(':') - 1);
+            return recortada.Split(' ').Select(int.Parse).ToArray();
+        }
+    }
+}
---
+++ Mate/medias.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mate
+{
+    class medias
+    {
+
+        /**
+         * Calcula y regresa la media aritmética
+         */
+        public static double mediaAritmetica(params int[] vals)
+        {
+
+            double promedio = 0;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                promedio += vals[i];
+            }
+
+            return promedio / vals.Length;
+        }
+
+        /**
+         * Calcula y regresa la raiz enésima = x^(1/n)
+         */
+        private static double raizEnesima(double x, int n)
+        {
+
+            return Math.Pow(x, (1 / (double)n));
+        }
+
+        /**
+         * Usa raizEnesima para calcular y regresar la media geométrica
+         */
+        public static double mediaGeometrica(params int[] vals) {
+
+            double media = 1;
+
+            for (byte i = 0; i < vals.Length; i++)
+                media *= vals[i];
+
+            return Math.Round(raizEnesima(media, vals.Length), 2);
+
+        }
+
+        /**
+         * Este método no está implementado
+         */
+        public static double mediaArmonica(params int[] vals)
+        {
+            throw new NotImplementedException();
+            //throw new System.ArgumentException("MÉTODO NO IMPLEMENTADO", "MÉTODO MEDIA ARMÓNICA");
+
+            /*
+            double sumainferior = 0;
+            double resultado = 0;
+
+            for (int i = 0; i < vals.Length; i++)
+            {
+                sumainferior += (1 / vals[i]);
+            }
+
+            resultado = vals.Length / sumainferior;
+            return resultado;
+
+
+            6:mediaArmonica:120 20 100 130:52.61
+
+            */
+        }
+    }
+}
